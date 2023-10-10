fde857e2eca5998c1d8407ccc7efacef53c0ace6
nomayen <nomayen.ohin@gmail.com>
2023-10-11 04:06:25 +0600

    create a modal

--- js/phone.js
+++ js/phone.js
@@ -37,8 +37,8 @@ const displayPhones = (phones, isShowAll) => {
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+                <button onClick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details </button>
             </div>
         </div>
         `
@@ -48,6 +48,13 @@ const displayPhones = (phones, isShowAll) => {
     }
 }
 
+
+const handleShowDetails = async (id) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json();
+    console.log(data);
+     
+} 
 
 // search 
 
